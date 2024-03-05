bf6052c78dbf3c1f50a5439824db0941abab684e
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-03-05 12:52:33 +0200

    created tampletes components

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,14 @@
+import Contact from '../Contact/Contact';
+
+const ContactList = () => {
+  return (
+    <div>
+      <Contact />
+      <Contact />
+      <Contact />
+      <Contact />
+    </div>
+  );
+};
+
+export default ContactList;
